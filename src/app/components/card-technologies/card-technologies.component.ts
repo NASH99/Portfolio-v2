bbd7c54cd44c5f7465e0ff1ae383a701a88e2a68
NASH <nash@MacBook-Pro-de-NASH-2.local>
2023-12-19 20:13:45 -0300

    add: principal structure

---
+++ src/app/components/card-technologies/card-technologies.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-technologies',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-technologies.component.html',
+  styleUrl: './card-technologies.component.scss'
+})
+export class CardTechnologiesComponent {
+
+}
