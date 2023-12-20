404457a8a75d748bf82c2f28100b91be73720e39
NASH <nash@MacBook-Pro-de-NASH-2.local>
2023-12-19 22:44:58 -0300

    add structure and styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
+import { AboutComponent } from './views/about/about.component';
+import { ProyectsComponent } from './views/proyects/proyects.component';
 
 export const routes: Routes = [
   {path: '',component:HomeComponent, title:'Home'}
+
 ];
--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../components/button/button.component';
+import { CardTechnologiesComponent } from '../../components/card-technologies/card-technologies.component';
+
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent,CardTechnologiesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
+  public TECHNOLOGIES_URL: string = 'https://pokeapi.co/api/v2/pokemon/';
+
+  constructor(private http:HttpClient){}
+
+  ngOnInit(): void{
+    this.searchData();
+  };
+
+  public searchData(): void{
+    this.http.get<void>(this.TECHNOLOGIES_URL).subscribe(
+      (data: any) => {
+        console.log(data);
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    )
+  }
 }
